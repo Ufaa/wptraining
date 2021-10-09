40a575fb53999ff906a28a106b722e61e8fc008a
Ufaa <dhuwanee.no1@gmail.com>
2021-10-09 15:01:33 +0900

    20211009

--- wp-content/themes/wp-theme/sidebar.php
+++ wp-content/themes/wp-theme/sidebar.php
@@ -1,11 +1,15 @@
 <aside class="sidebar">
+  <div class="banner">
+    <a href="https://coachtech.site">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/banner.png">
+    </a>
+  </div>
+</aside>
+
+<!--
   <?php if (is_active_sidebar('main-sidebar')) : ?>
     <ul class="menu">
       <?php dynamic_sidebar('main-sidebar'); ?>
     </ul>
   <?php endif; ?>
-</aside>
-
-<!--
-
   -->
